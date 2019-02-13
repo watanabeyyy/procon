8e60a4ae3eb8bba9aa32ffeaa89f93492af2f40c
Yuki Watanabe <yuki.watanabe@furukawaelectric.com>
2019-02-13 12:17:57 +0900

    add

--- AtCoder/ABC/ABC117/d.cpp
+++ AtCoder/ABC/ABC117/d.cpp
@@ -1,9 +1,4 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <ios>
-#include <iomanip>
-#include <bitset>
+#include <bits/stdc++.h>
 
 using namespace std;
 typedef long long ll;

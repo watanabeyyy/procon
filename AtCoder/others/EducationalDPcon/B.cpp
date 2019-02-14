2d376649297e250932164e1cfa8726d5ef0b06e8
Yuki Watanabe <yuki.watanabe@furukawaelectric.com>
2019-02-15 08:29:53 +0900

    add

--- AtCoder/others/EducationalDPcon/B.cpp
+++ AtCoder/others/EducationalDPcon/B.cpp
@@ -14,9 +14,17 @@ int main()
     }
     ll dp[100001] = {0};
     dp[1] = abs(h[1] - h[0]);
+    ll temp;
     for (int i = 2; i < N; i++)
     {
-        dp[i] = min(dp[i - 1] + abs(h[i] - h[i - 1]), dp[i - 2] + abs(h[i] - h[i - 2]));
+        temp = dp[i - 1] + abs(h[i] - h[i - 1]);
+        for (int j = 0; j < K - 1; j++)
+        {
+            temp = min(temp, dp[i - j - 2] + abs(h[i] - h[i - j - 2]));
+            if ((i - j) == 2)
+                break;
+        }
+        dp[i] = temp;
     }
     cout << dp[N - 1] << endl;
     // system("pause");

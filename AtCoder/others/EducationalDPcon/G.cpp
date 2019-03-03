d5ebe82c033f4eeba9ef45fdc56cbc7c55aa09e8
Yuki Watanabe <yuki.watanabe@furukawaelectric.com>
2019-03-03 15:00:55 +0900

    add

--- AtCoder/others/EducationalDPcon/G.cpp
+++ AtCoder/others/EducationalDPcon/G.cpp
@@ -40,7 +40,47 @@ inline bool chmin(T &a, T b)
     return false;
 }
 
+int N, M;
+
+vi nodes[100001];
+int dp[100001];
+
+int dfs(ll idx)
+{
+    if (dp[idx] != -1)
+    {
+        return dp[idx];
+    }
+    else
+    {
+        int maxima = 0;
+        REP(i, nodes[idx].size())
+        {
+            maxima = max(maxima, dfs(nodes[idx][i]) + 1);
+        }
+        dp[idx] = maxima;
+        return maxima;
+    }
+}
+
 int main()
 {
+    cin >> N >> M;
+    REP(i, N + 1)
+    {
+        dp[i] = -1;
+    }
+    int temp_x, temp_y;
+    REP(i, M)
+    {
+        cin >> temp_x >> temp_y;
+        nodes[temp_x].push_back(temp_y);
+    }
+    int max_L = 0;
+    REP(i, N + 1)
+    {
+        max_L = max(max_L, dfs(i));
+    }
+    cout << max_L;
     // system("pause");
 }

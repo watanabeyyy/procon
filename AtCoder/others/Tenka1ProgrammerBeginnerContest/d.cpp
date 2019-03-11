c71759fc1d679a43594f232027dd2ff0c59beff4
watanabeyyy <yuki.watanabe@furukawaelectric.com>
2019-03-12 00:17:44 +0900

    add

--- AtCoder/others/Tenka1ProgrammerBeginnerContest/d.cpp
+++ AtCoder/others/Tenka1ProgrammerBeginnerContest/d.cpp
@@ -1,7 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-#define int ll
 #define REP(i, n) FOR(i, 0, n)
 #define FOR(i, a, b) for (ll i = a; i < b; i++)
 #define UM unordered_map
@@ -37,7 +36,44 @@ inline bool chmin(T &a, T b)
     return false;
 }
 
+int lst[500][500];
 signed main()
 {
+    int N;
+    cin >> N;
+
+    ll tmp = -1;
+    FOR(i, 1, 500)
+    {
+        if (i * (i + 1) == 2 * N)
+            tmp = i + 1;
+    }
+    if (tmp == -1)
+    {
+        cout << "No" << endl;
+        return 0;
+    }
+    N = tmp;
+    cout << "Yes" << endl;
+    cout << N << endl;
+    tmp = 1;
+    REP(i, N)
+    {
+        FOR(j, 1, N - i)
+        {
+            lst[i][i + j - 1] = tmp;
+            lst[i + j][i] = tmp;
+            tmp++;
+        }
+    }
+    REP(i, N)
+    {
+        cout << N - 1;
+        REP(j, N - 1)
+        {
+            cout << " " << lst[i][j];
+        }
+        cout << endl;
+    }
     // system("pause");
 }

122af374230bf760dce0c5f94836d4ddd45b419d
watanabeyyy <yuki.watanabe@furukawaelectric.com>
2019-03-16 16:20:43 +0900

    add

---
+++ AtCoder/ABC/ABC114/A.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define int ll;
+#define REP(i, n) FOR(i, 0, n)
+#define FOR(i, a, b) for (ll i = a; i < b; i++)
+#define UM unordered_map
+#define ALL(a) (a).begin(), (a).end()
+typedef vector<ll> vi;
+typedef vector<vector<ll>> vvi;
+typedef pair<ll, ll> pii;
+const long long INF = 1LL << 58;
+struct Edge
+{
+    ll s, t, d;
+};
+typedef vector<vector<Edge>> Graph;
+typedef vector<pii> vpii;
+template <class T>
+inline bool chmax(T &a, T b)
+{
+    if (a < b)
+    {
+        a = b;
+        return true;
+    }
+    return false;
+}
+template <class T>
+inline bool chmin(T &a, T b)
+{
+    if (a > b)
+    {
+        a = b;
+        return true;
+    }
+    return false;
+}
+
+signed main()
+{
+    ll N;
+    cin >> N;
+    if (N == 7 || N == 5 || N == 3)
+        cout << "YES" << endl;
+    else
+        cout << "NO" << endl;
+}
